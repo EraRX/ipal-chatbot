75f3b3ae21caa818ddf08a500b8d199a1578d6aa
EraRX <deras@home.nl>
2025-07-30 16:31:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -233,6 +233,7 @@ Dat is eenvoudig! Zorg ervoor dat uw melding duidelijk is:
 • Beschrijf het probleem zo gedetailleerd mogelijk.  
 • Voegt u geen document toe, zet dan het documentformaat in het ticket op “geen bijlage”.  
 • Geef uw telefoonnummer op waarop wij u kunnen bereiken, zodat de helpdesk contact met u kan opnemen.
+
 • Stel hier uw vraag:
 '''
         pdf_data = make_pdf(
