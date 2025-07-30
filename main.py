15aa2fb66b85a306b7c53ca1806ac835aec877cb
EraRX <deras@home.nl>
2025-07-30 16:28:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -126,11 +126,9 @@ def make_pdf(question: str, answer: str, ai_info: str) -> bytes:
     story.append(Spacer(1, 6))
     story.append(Paragraph("• Voegt u geen document toe, zet dan het documentformaat in het ticket op “geen bijlage”.", body_style))
     story.append(Spacer(1, 6))
-    story.append(Paragraph("• Geef uw telefoonnummer op waarop wij u kunnen bereiken, zodat de helpdesk contact met u kan opnemen.", body_style))
+    story.append(Paragraph("• Geef uw telefoonnummer op waarop wij u kunnen bereiken, zodat de helpdesk contact met u kan opnemen. STEL HIER UW VRAAG:", body_style))
     story.append(Spacer(1, 6))
-    story.append(Paragraph("• Stel hier uw vraag:", body_style))
-    story.append(Spacer(1, 6))
-        buffer.seek(0)
+    buffer.seek(0)
     return buffer.getvalue()
 
 @st.cache_data
